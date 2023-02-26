498c4c0322ba7692bbd7a55f31dd3cd8fbb7b1cb
Andrija Urosevic <andrija.ada99@gmail.com>
2023-02-26 18:24:44 +0100

    Update: tum_exercises 04 homework

--- isabelle/tum_exercises/Hw04.thy
+++ isabelle/tum_exercises/Hw04.thy
@@ -9,14 +9,20 @@ text \<open>We define @{text "ptrees"}, which are trees that store the popularit
 
 type_synonym 'a ptree = "(nat * 'a) tree"
 
+definition test_ptree :: "nat ptree" where
+  "test_ptree \<equiv> \<langle>\<langle>\<langle>\<rangle>, (1, 2), \<langle>\<rangle>\<rangle>, (2, 5) ,\<langle>\<langle>\<langle>\<rangle>, (1, 6) ,\<langle>\<rangle>\<rangle>, (3, 7), \<langle>\<langle>\<rangle>, (3, 8),\<langle>\<rangle>\<rangle>\<rangle>\<rangle>"
+
 text \<open>Define the set of elements of that tree as a recursive function, then show it 
-      correct w.r.t. to the normal @{text "set_tree"} (‘ is the set @{text "image"}):\<close> 
+      correct w.r.t. to the normal @{text "set_tree"} (` is the set @{text "image"}):\<close> 
 
 fun set_ptree :: "('a::linorder) ptree \<Rightarrow> 'a set" where
-  "set_ptree Leaf = {}"
+  "set_ptree Leaf         = {}"
 | "set_ptree (Node l a r) = (set_ptree l)
                           \<union> {snd a}
                           \<union> (set_ptree r)"
+
+value "set_ptree test_ptree"
+value "snd ` set_tree test_ptree"
 
 lemma set_ptree: "set_ptree t = snd ` set_tree t"
   by (induction t) auto
@@ -27,22 +33,26 @@ text \<open>Define the binary search tree predicate as well as insert function f
 text \<open>If a node is already present, overwrite the old popularity value\<close>
 
 fun pbst :: "'a::linorder ptree \<Rightarrow> bool" where
-  "pbst Leaf \<longleftrightarrow> True"
-| "pbst (Node l a r) \<longleftrightarrow> (\<forall> x \<in> set_tree l. snd x < snd a) 
-                       \<and> (\<forall> x \<in> set_tree r. snd a < snd x) 
+  "pbst Leaf         \<longleftrightarrow> True"
+| "pbst (Node l a r) \<longleftrightarrow> (\<forall> x \<in> set_ptree l. x < snd a) 
+                       \<and> (\<forall> x \<in> set_ptree r. snd a < x) 
                        \<and> pbst l \<and> pbst r"
 
+value "pbst test_ptree"
+
 fun pins :: "(nat * 'a::linorder) \<Rightarrow> 'a ptree \<Rightarrow> 'a ptree" where
-  "pins x Leaf = Node Leaf x Leaf"
+  "pins x Leaf         = Node Leaf x Leaf"
 | "pins x (Node l a r) = (if snd x < snd a then Node (pins x l) a r else
-                          if snd x > snd a then Node l a (pins x r) else
-                          Node l x r) "
+                          if snd a < snd x then Node l a (pins x r) else
+                          Node l (fst x, snd a) r) "
+
+value "pins (2, 9) test_ptree"
 
 text \<open>Show the most interesting property, namely that insert preserves the invariant:\<close>
 
 lemma pins_invar: "pbst t \<Longrightarrow> pbst (pins x t)"
   apply (induction x t rule: pins.induct)
-   apply auto
+   apply (auto split: prod.split)
   sorry
 
 text \<open>Now define the @{text "pisin"} function, which should return the updated @{text "ptree"} 
@@ -51,20 +61,31 @@ text \<open>Now define the @{text "pisin"} function, which should return the upd
 
 fun pisin :: "'a :: linorder \<Rightarrow> 'a ptree \<Rightarrow> ('a ptree * nat)" where
   "pisin x Leaf = Pair Leaf 0"
-| "pisin x (Node l a r) = (let left = pisin x l; right = pisin x r in 
-                          (if x < snd a then Node (pisin x l) a r else
-                           if x > snd a then Node l a (pisin x r) else
-                           Node l a r)"
+| "pisin x (Node l a r) = 
+    (let (lt, lc) = pisin x l;
+         (rt, rc) = pisin x r;
+            count = fst a
+      in (if x < snd a then (Node lt a r, lc) else
+          if snd a < x then (Node l a rt, rc) else
+          ((Node l (count + 1, snd a) r), count)))"
+
+value "test_ptree"
+value "pisin 2 test_ptree"
 
 text \<open>Show the correctness of your function:\<close>
 
 lemma pisin_set: "pbst t \<Longrightarrow> set_ptree (fst (pisin x t)) = set_ptree t"
-  sorry
+  by (induction x t rule: pisin.induct) (auto split: prod.split if_split_asm)
 
 lemma pisin_invar: "pbst t \<Longrightarrow> pbst (fst (pisin x t))"
-  sorry
+  apply (induction x t rule: pisin.induct)
+   apply (auto split: prod.split)
+   apply (metis fst_eqD pisin_set)+
+  done
 
 lemma pisin_inc: "pbst t \<Longrightarrow> (n,x) \<in> set_tree t \<Longrightarrow> (Suc n,x) \<in> set_tree (fst (pisin x t))"
+  apply (induction x t arbitrary: n rule: pisin.induct)
+   apply (auto split: prod.split)
   sorry
 
 text \<open>Knowing the popularity of element queries, we can re-order the tree from time to time to
@@ -76,14 +97,22 @@ text \<open>Implement such a re-ordering — it does not need to be optimal, but
 text \<open>Hint: Sorting might be useful. Have a look at the pre-defined sort function and its
       implementation.\<close>
 
-term sort
+term "sort_key (\<lambda> x. fst x) (inorder (t::'a ptree))"
+term "sort \<circ> inorder"
+term pins
 
 definition reorder :: "('a :: linorder) ptree \<Rightarrow> 'a ptree" where
-  "reorder _ = undefined"
+  "reorder t = foldr pins (sort_key fst (inorder t)) Leaf"
+
+value "reorder test_ptree"
+
+value "sort_key fst [(1::nat, 2::nat), (3, 2)]"
 
 text \<open>Show that your re-ordering preserves the invariant:\<close>
 
 theorem reorder_pbst: "pbst t \<Longrightarrow> pbst (reorder t)"
+  apply (induction t)
+   apply auto
   sorry
 
 section \<open>Popularity Annotated Trees (II)\<close>
